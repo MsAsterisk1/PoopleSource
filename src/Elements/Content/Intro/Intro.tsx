a955360f346e8b9327b5e3a9531f9e2373757b52
unknown <msasterisk2@gmail.com>
2025-09-03 15:33:06 -0500

    add poople icon to index.html so hopefully google picks it up

--- src/Elements/Content/Intro/Intro.tsx
+++ src/Elements/Content/Intro/Intro.tsx
@@ -8,14 +8,12 @@ type IntroProps = {
 export function Intro(props: IntroProps) {
     return (
         <div className="Intro">
-
             <p><b>Get to "POOP" in as few steps as possible</b></p>
             <p>Each word must be exactly one letter different from the last:</p>
             <img src={example}
                  alt={`The words "barn" and "born", highlighting the one letter where they differ`}/>
             <p>A random starting word is chosen daily. Can you find the shortest path?</p>
             <button className={"IntroPlayButton"} onClick={props.onClosePress}>Click here to play!</button>
-
         </div>
     );
 }
