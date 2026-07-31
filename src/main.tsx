83d9a427024158d6d08e2631c35a6f866b22b5fe
MsAsterisk1 <msasterisk2@gmail.com>
2026-07-31 14:48:54 -0500

    fix import

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import './main.css';
 import App from './Elements/App';
 import {BrowserRouter, Route, Routes} from "react-router";
 import Support from "./Support.tsx";
-import Privacy from "./Privacy.tsx";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
