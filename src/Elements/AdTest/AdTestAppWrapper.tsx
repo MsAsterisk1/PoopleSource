69aae134c1110f462c69d49b833291d79f33e8e1
MsAsterisk1 <msasterisk2@gmail.com>
2026-09-18 12:00:25 -0500

    gate console log on test

--- src/Elements/AdTest/AdTestAppWrapper.tsx
+++ src/Elements/AdTest/AdTestAppWrapper.tsx
@@ -2,7 +2,9 @@ import PubliftAppWrapper from "./PubliftAppWrapper.tsx";
 import EzoicAppWrapper from "./EzoicAppWrapper.tsx";
 
 export default function AdTestAppWrapper(props: {test?: boolean}) {
-    console.log("Getting provider from React: " + sessionStorage.getItem("adProvider"))
+    if (props.test) {
+        console.log("Getting provider from React: " + sessionStorage.getItem("adProvider"))
+    }
 
     return (
             sessionStorage.getItem("adProvider") === "ezoic" ?
